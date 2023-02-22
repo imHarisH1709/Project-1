60e18dc407c03edf7d5bed0ad1f4104802ac335d
imHarisH <118507091+imHarisH1709@users.noreply.github.com>
2023-02-22 12:32:53 +0530

    Add files via upload

---
+++ harish1/src/main/java/com/example/demo/Harish1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Harish1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Harish1Application.class, args);
+	}
+
+}
